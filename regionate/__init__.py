363369c0e1dde0e39db6bc01a21d2be47a5fb134
Henri Drake <henrifdrake@gmail.com>
2023-09-08 12:25:35 -0400

    Implement dynamic versioning
    This way version can be easily accessed from within python with:
    `regionate.__version__`

--- regionate/__init__.py
+++ regionate/__init__.py
@@ -3,3 +3,4 @@ from .regions import *
 from .grid_conform import *
 from .utilities import *
 from .integrate import *
+from .version import __version__
---
+++ regionate/version.py
@@ -0,0 +1,3 @@
+"""regionate: version information"""
+
+__version__ = "0.0.1"
